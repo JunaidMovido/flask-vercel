017ce05bb670b5445012a08af45cf7b96c5f0863
JunaidMovido <jh@movido-media.com>
2024-03-06 20:41:57 +0100

    solved file-access issue on vercel

--- api/logger.py
+++ api/logger.py
@@ -4,17 +4,12 @@ from api.config import Config
 logger = logging.getLogger('logger')
 logger.setLevel(logging.DEBUG)
 
-# Create a file handler and set the logging level
-file_handler = logging.FileHandler('app.log')
-file_handler.setLevel(logging.DEBUG)
-
 # Create a console handler and set the logging level
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)
 
 # Create a formatter and attach it to the handlers
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-file_handler.setFormatter(formatter)
 console_handler.setFormatter(formatter)
 
 # Add the handlers to the logger
@@ -22,4 +17,8 @@ logger.addHandler(console_handler)
 
 # The vercel env is readonly - so to prevent this
 if Config.FLASK_ENVIRONMENT == "development":
+    # Create a file handler and set the logging level
+    file_handler = logging.FileHandler('app.log')
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
